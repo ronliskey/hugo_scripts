76706281b2b1fe0da272fd5c7c9b0ffff4fb6df6
Ron Liskey <ronliskey@gmail.com>
2017-08-03 01:46:17 -0700

    cleaned up code, added abort on existing  directory

--- gallerymaker.sh
+++ gallerymaker.sh
@@ -2,98 +2,84 @@
 
 function usage()
 {
-  echo "Sets up a directory of images for use in Hugo Easy Gallery."
-  echo "Copy resulting files into a gallery directory in your Hugo site."
-  echo $0 "-d [output_directory]"
-  echo "Optional arguments: "
-  echo "\t-d Name of output directory"
-  echo "\t-v Verbose"
-  echo "\t-h --help"
-  echo ""
+  echo "Usage: `basename $0` options (-d output_dir) (-t -thumbname) -h for help"
+  exit
+}
+
+function dir_error()
+{
+    echo "\nERROR: That directory already exists!"
+    usage
 }
 
 IMAGE_WIDTH=1200
 THUMB_WIDTH=300
-THUMB_NAME=-thumb
+THUMB_NAME="-thumb"
 OUTPUT_DIR="gallery_files"
-VERBOSE=0
 
-while getopts d:vh? option
+while getopts d:t:h? option
 do
   case "${option}"
   in
   d) OUTPUT_DIR=${OPTARG};;
-  v) VERBOSE=$OPTARG;;
-  h|?) HELP=$OPTARG;;
+  t) THUMB_NAME=${OPTARG};;
+  h|?) usage;;
   esac
 done
 
-#
-#
-# Thumbnail filenames must match main filenames.
-#    <main>.jpg
-#    <main>-thumb.jpg
-#
-
-clear
-
-echo "\nRenaming files with U/C JPG..."
 for pic in *".JPG"
 do
 	if [[ -f "$pic" ]]
 	then
-	    mv "$pic" "${pic%.JPG}.jpg"
-	    echo "\t $pic"
+	    mv "${pic}" "${pic%.JPG}.jpg"
 	fi
 done
 
-
-echo "\nRenaming files with U/C PNG..."
 for pic in *".PNG"
 do
-	if [[ -f "$pic" ]]
+	if [[ -f "${pic}" ]]
 	then
-	    mv "$pic" "${pic%.PNG}.png"
-	    echo "\t $pic"
-	fi
+	    mv "${pic}" "${pic%.PNG}.png"
+	fi  
 done
 
-echo "\nCreating directory: $OUTPUT_DIR"
-mkdir -p $OUTPUT_DIR 
+# Abort if directory already exists.
+if [ -d "${OUTPUT_DIR}" ]; then
+    dir_error
+fi
 
-echo "\nResizing images..."
+mkdir -p ${OUTPUT_DIR}
+
+# Create resized image files.
 for pic in *.jpg
 do
-	convert $pic -resize $IMAGE_WIDTH  $OUTPUT_DIR/$pic
-	echo "\t $pic"
+	convert ${pic} -resize ${IMAGE_WIDTH}  ${OUTPUT_DIR}/${pic}
 done
 
-echo "\nCreating thumbnails..."
+# Create thumbnail files.
 for pic in *.jpg
 do
-	cp "$pic" "$OUTPUT_DIR/${pic%.jpg}$THUMB_NAME.jpg"
-	echo "\t $pic"
+	cp "$pic" "${OUTPUT_DIR}/${pic%.jpg}${THUMB_NAME}.jpg"
 done
 
-echo "\nResizing thumbnails..."
-for pic in $OUTPUT_DIR/*-thumb.jpg
+# Resize thumbnail files.
+for pic in ${OUTPUT_DIR}/*${THUMB_NAME}.jpg
 do
-	convert $pic -resize $THUMB_WIDTH $pic
-	echo "\t $pic"
+	convert ${pic} -resize ${THUMB_WIDTH} $pic
 done
 
-echo "\nWorking in directory: $OUTPUT_DIR..."
-cd $OUTPUT_DIR 
+cd $OUTPUT_DIR
 
-echo "\nAdding index file."
+# Add index file to block directory view.
 touch index.html
 
-echo "\nSetting read file permissions..."
-chmod go+r *.* 
+# Ensure all files have correct read permissions.
+chmod go+r *.*
 
-echo "\nDeleting quarantine bits..."
-# xattr -dr com.apple.quarantine *.*
-find . -iname '*-thumb.jpg' -print0 | xargs -0 xattr -d com.apple.quarantine
-find . -iname '*-thumb.png' -print0 | xargs -0 xattr -d com.apple.quarantine
+# Remove quarentine atribute if present. (OSX only)
+find . -iname '*${THUMB_NAME}.jpg' -print0 | xargs -0 xattr -dr com.apple.quarantine
+find . -iname '*${THUMB_NAME}.png' -print0 | xargs -0 xattr -dr com.apple.quarantine
 
-echo "\n Fini! \n"
+clear
+pwd
+ls -l
